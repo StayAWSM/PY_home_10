bca3b8e339fb1b033aec6ed8a327dcd1b6999882
StayAWSM <ytaken@mail.ru>
2022-12-29 21:20:25 +0300

    Бот-калькулятор с логгированием завершен.

--- py_bot.py
+++ py_bot.py
@@ -24,14 +24,10 @@ logger.addHandler(handler)
 @bot.message_handler(commands=['start'])
 def start(message):
     text_message1 = f'<b><i>Привет, {message.from_user.first_name}!</i></b> \U0001F600'
-    text_message2 = f'Этот бот предназначен для выполнения математических операций,\n '\
-                    f'а также же просмотра расслабляющего видео и помощи!'
-    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    # start = types.KeyboardButton('Start')
-    # markup.add(start)
+    text_message2 = f'Этот бот предназначен для выполнения математических операций,\n ' \
+                    f'а также же просмотра истории 5 последних действий'
     bot.send_message(message.chat.id, text_message1, parse_mode='html')
     bot.send_message(message.chat.id, text_message2, parse_mode='html')
-
 
 
 @bot.message_handler(content_types=['text'])
@@ -44,21 +40,23 @@ def mess(message):
 
     if 'start' in get_message_bot:
         bot.send_message(message.chat.id, 'Вводимое не должно быть командой', reply_markup=markup)
+    elif 'посмотреть историю' in get_message_bot:
+        history(message)
     else:
         try:
             calc = eval(get_message_bot)
             bot.send_message(message.chat.id, f'{get_message_bot} = {calc}', reply_markup=markup)
-            json_work.write(message.chat.id, f'{get_message_bot} = {calc}', message.from_user.first_name, reply_markup=markup)
-        except BaseException:
+            json_work.write(message.chat.id, f'{get_message_bot} = {calc}', message.from_user.first_name)
+        except SyntaxError:
             bot.send_message(message.chat.id, 'Введите математическое выражение! Например, 2 + 2', reply_markup=markup)
+
 
 @bot.message_handler(commands=['history'])
 def history(message):
-    get_message_bot = message.text.strip().lower()
-    if 'Посмотреть историю' in get_message_bot:
-        temp_list = json_work.read(message.chat.id)
-        for item in temp_list:
-            bot.send_message(message.chat.id, item)
+    temp_list = json_work.read(message.chat.id)
+    for item in temp_list:
+        bot.send_message(message.chat.id, item)
+
 
 print('Бот работает')
 
